9644c71c2048b3e90415bbfe8b391339597f9aaf
leann.lindsey@utah.edu <leann.lindsey@utah.edu>
2023-11-03 14:00:06 -0700

    changed file names and added data

--- test_script.sh
+++ test_script.sh
@@ -8,7 +8,6 @@ file_21="localassm_extend_7-21.dat"
 file_33="localassm_extend_1-33.dat"
 file_55="localassm_extend_7-55.dat"
 file_77="localassm_extend_9-77.dat"
-data_dir="/global/homes/l/lindsey/WORK_INTERNSHIP_2023/gpu_local_ht/locassm_data"
 kmer_sizes=(21 33 55 77)
 i=0
 test_number=$1
@@ -18,12 +17,12 @@ do
 	echo "************************************"
 	echo "Running test for Kmer size: $kmer_size"
 	echo "Running test on $data_dir/$curr_file"
-	./a.out $data_dir/$curr_file $kmer_size out_file_$kmer_size.dat > screen_$kmer_size
-	#echo "./a.out $data_dir/$curr_file $kmer_size out_file_$kmer_size.dat > screen_$kmer_size"
+	./a.out ./locassm_data/$curr_file $kmer_size out_file_$kmer_size.dat > screen_$kmer_size
+	#echo "./a.out ./locassm_data/$curr_file $kmer_size out_file_$kmer_size.dat > screen_$kmer_size"
 	benchmark_time=$(tail -n 1 screen_$kmer_size)
         echo "time for $kmer_size on test number $test_number: $benchmark_time" 
 	sort out_file_$kmer_size.dat >> sorted_new
-	sort $data_dir/"res_$curr_file" >> sorted_res
+	sort ./locassm_data/"res_$curr_file" >> sorted_res
 	difference=$(diff sorted_new sorted_res)
 
 	echo "Number of differences in $kmer_size"
